cac244346a3145a2df75e5956e025b7be187d0df
linhld <linhld@its-global.vn>
2020-07-06 15:53:42 +0700

    init app

---
+++ app/Validators/CreateJob.js
@@ -0,0 +1,26 @@
+'use strict'
+
+class CreateJob {
+  get rules () {
+    return {
+      title: 'required',
+      link: 'required'
+      // validation rules
+    }
+  }
+
+  get messages() {
+    return {
+      'required': 'Hold up, the {{ field }} is required.'
+    }
+  }
+
+  async fails(error) {
+    this.ctx.session.withErrors(error)
+        .flashAll();
+
+    return this.ctx.response.redirect('back');
+  }
+}
+
+module.exports = CreateJob
